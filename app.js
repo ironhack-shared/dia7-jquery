be864d68c3ed371aa76865568ac8626f9ad2cf51
daniel vicario <danigv.email@gmail.com>
2018-10-30 11:54:58 +0100

    added dynamic title

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var App = {
         })
 
         let selector = ".mobiles-container .mobile-phone";
-        $(selector).html(`<h4>Phone 1</h4>
-        <img src="https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter" alt="">`);
+        $(selector).each(function (index) {
+            $(this).html(`<h4>Phone ${index + 1}</h4>
+            <img src="https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter" alt="">`)
+        })
 
     },
     createJewels: function () {
