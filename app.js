724d9d946cf8d596592b514e206282025defb495
daniel vicario <danigv.email@gmail.com>
2018-10-30 11:04:40 +0100

    splitted the code into a separate js file which initiates the app

---
+++ app.js
@@ -0,0 +1,10 @@
+var App = {
+    init : function () {
+        $(".testButton").click(function (miEvento) {
+            $(this).fadeOut();
+        })
+    },
+    createJewels: function () {
+      // code for jewels
+    }
+  }
