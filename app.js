b68bdf3ef5cb977b6a32190632898ef7b00a78e3
daniel vicario <danigv.email@gmail.com>
2018-10-30 11:51:45 +0100

    added dynamic contents

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var App = {
             $(this).fadeOut();
         })
 
-        let selector = ".mobiles-container .mobile-phone:eq(1) h4";
-        $(selector).html("<span>Hola</span> <span class='important'>important!</span>");
+        let selector = ".mobiles-container .mobile-phone";
+        $(selector).html(`<h4>Phone 1</h4>
+        <img src="https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter" alt="">`);
 
     },
     createJewels: function () {
