356041f03f573a98ccbb05f11147bff2ca6644ae
daniel vicario <danigv.email@gmail.com>
2018-10-30 12:40:46 +0100

    added some class manipulation for liking!

--- app.js
+++ app.js
@@ -11,21 +11,29 @@ var App = {
             }
         }
 
-        // var phones = [new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone"), new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone"), new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone")]
+        var phones = [new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone"), new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone"), new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone")]
 
-        // for (var i = 0; i < phones.length; i++) {
-        //     $(".mobiles-container").append(`<div class="mobile-phone"></div>`)
-        // }
+        for (var i = 0; i < phones.length; i++) {
+            $(".mobiles-container").append(`<div class="mobile-phone"></div>`)
+        }
 
-        // let selector = ".mobiles-container .mobile-phone";
-        // $(selector).each(function (index) {
-        //     $(this).html(`<h4>${phones[index].productName}</h4>
-        //     <img src="${phones[index].img}" alt="">`)
-        // })
+        let selector = ".mobiles-container .mobile-phone";
+        $(selector).each(function (index) {
+            $(this).html(`<h4>${phones[index].productName}</h4>
+            <img src="${phones[index].img}" alt="">`)
+        })
 
-        var arrNames = ["sonia", "maria", "diego", "julio"]
+        // var arrNames = ["sonia", "maria", "diego", "julio"]
 
-        $("#username").attr("placeholder", arrNames[Math.floor(Math.random() * arrNames.length)])
+        // $("#username").attr("placeholder", arrNames[Math.floor(Math.random() * arrNames.length)])
+
+        if ($(".mobile-phone:eq(3)").hasClass("odd")) {
+            console.log("the user has not interacted yet with this area")
+        }	else {
+             console.log("DOM  area is dirty")   
+        }
+
+        $(".mobile-phone:eq(1)").hasClass("odd")
 
     },
     createJewels: function () {
---
+++ lines.js
@@ -0,0 +1,3 @@
+$("body").html("").append("<div></div>")
+
+$("div").height(Math.random() * 100).width(500).css("background-color", "red")
