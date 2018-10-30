f26f8f31755807bf6aa17b21640a64fc03b00f53
daniel vicario <danigv.email@gmail.com>
2018-10-30 12:04:47 +0100

    added API emulation

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ var App = {
             $(this).fadeOut();
         })
 
+        class Phone {
+            constructor (img, productName) {
+                this.img = img;
+                this.productName = productName;
+            }
+        }
+
+        var phones = [new Phone("https://cdn2.gsmarena.com/vv/bigpic/motorola-one-.jpg", "motorola"), new Phone("https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter", "iphone")]
+
+        for (var i = 0 ; i < phones.length; i++) {
+            $(".mobiles-container").append(`<div class="mobile-phone"></div>`)
+        }
+
         let selector = ".mobiles-container .mobile-phone";
         $(selector).each(function (index) {
-            $(this).html(`<h4>Phone ${index + 1}</h4>
-            <img src="https://images-cdn.azureedge.net/azure/in-resources/0211f4bd-ce6a-4162-95a2-f801e5ae9176/Images/ProductImages/Source/V9%204GB%2064GB%20Black%20-%20Vivo.jpg;width=300;height=450;scale=canvas;anchor=bottomcenter" alt="">`)
+            $(this).html(`<h4>${phones[index].productName}</h4>
+            <img src="${phones[index].img}" alt="">`)
         })
 
     },
