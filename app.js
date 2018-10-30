80c0d0e6c65c3db8a8a6934146907631732cf1e1
daniel vicario <danigv.email@gmail.com>
2018-10-30 11:46:55 +0100

    added dynamic text on a specific selector

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var App = {
         $(".testButton").click(function (miEvento) {
             $(this).fadeOut();
         })
+
+        let selector = ".mobiles-container .mobile-phone:eq(1) h4";
+        $(selector).html("<span>Hola</span> <span class='important'>important!</span>");
+
     },
     createJewels: function () {
       // code for jewels
